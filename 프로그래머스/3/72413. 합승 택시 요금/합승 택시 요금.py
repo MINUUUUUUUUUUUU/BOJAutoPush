2311e9b232015752e33a3543e52cbdb0e057e190
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2025-07-04 13:16:01 +0900

    [level 3] Title: 합승 택시 요금, Time: 1955.24 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,5 +1,7 @@
+import heapq
+
 def solution(n, s, a, b, fares):
-    answer = 0
+    answer = 1e9
     
     graph = [[] for _ in range(n+1)]
 
@@ -7,49 +9,47 @@ def solution(n, s, a, b, fares):
     for u,v,w in fares:
         graph[u].append((v, w))
         graph[v].append((u, w))
-    
-    def smallest(distance, visited):
-        min_val = 1e8
-        index = 0
-        for i in range(1, n+1):
-            if distance[i] < min_val and not visited[i]:
-                min_val = distance[i]
-                index = i
-        return index
         
+    
     def dijkstra(start):
-        visited = [False for _ in range(n+1)]
-        distance = [1e8 for _ in range(n+1)]
-        
-        distance[start] = 0
-        visited[start] = True
-    
-        for i in graph[start]:
-            distance[i[0]] = i[1]
-            
-        for i in range(n-1):
-            now = smallest(distance, visited)
-            visited[now] = True
-            
-            for j in graph[now]:
-                cost = distance[now] + j[1]
-                if cost < distance[j[0]]:
-                    distance[j[0]] = cost
+        queue = []
+        distance = [1e9 for _ in range(n+1)]
+        heapq.heappush(queue, [0, start])
+
+        while queue:
+            dist, now = heapq.heappop(queue)
+
+            if dist > distance[now]:
+                continue
+
+            for next, w in graph[now]:
+                cost = dist + w
+
+                if distance[next] > cost:
+                    distance[next] = cost
+                    heapq.heappush(queue, [cost, next])
                     
         return distance
+                
+    ## 따로따로
+    dis1 = dijkstra(s)
+    answer = min(answer, dis1[a] + dis1[b])
     
-    cases = set()
-    
-    ## 각자 집가는 경우
-    tmp = dijkstra(s)
-    cases.add(tmp[a] + tmp[b])
-    
-    # A or B 끼리 i까지 같이 가고 따로 집감
+    ## i까지 갔다가 쫑
     for i in range(1, n+1):
-        tmp_val = tmp[i]
-        tmp2 = dijkstra(i)
-        tmp_val += tmp2[a] + tmp2[b]
-        cases.add(tmp_val)
+        
+        if s == i:
+            continue
+            
+        dis2 = dijkstra(s)
+        dis3 = dijkstra(i)
+        answer = min(answer, dis2[i] + dis3[a] + dis3[b])
+
+    ## a or b를 들렀다 나머지 하나
+    dis4 = dijkstra(s)
+    dis5 = dijkstra(a)
+    dis6 = dijkstra(b)
     
-    # print(cases)
-    return min(cases)
+    answer = min(answer, dis4[a] + dis5[b], dis4[b] + dis6[a])
+    
+    return answer
